17e43f8a9c422f50dbcea0dee557e5cd698bb1b5
Rishav Sharam <rishav.sharma.18@mountblue.tech>
2022-01-26 18:18:11 +0530

    HOST change

--- twitterclone/settings.py
+++ twitterclone/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['twitter-django-clone.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['twitter-django-clone.herokuapp.com']
 
 
 # Application definition
